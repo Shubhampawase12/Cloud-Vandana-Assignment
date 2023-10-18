8cbbb8e2a1062879fdf99b002dda2037a6f4ab42
Shubham Pawase <118187208+Shubhampawase12@users.noreply.github.com>
2023-10-18 20:36:13 +0530

    Add files via upload

---
+++ RomanToInteger.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine().toUpperCase();
+
+        int result = romanToInteger(romanNumeral);
+        System.out.println("The integer equivalent of " + romanNumeral + " is: " + result);
+    }
+
+    public static int romanToInteger(String roman) {
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = roman.length() - 1; i >= 0; i--) {
+            char numeral = roman.charAt(i);
+            int value = romanMap.get(numeral);
+
+            if (value < prevValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+
+            prevValue = value;
+        }
+
+        return result;
+    }
+}
+
